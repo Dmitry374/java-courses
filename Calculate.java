43497cd929ec12b663e92017a8989e983ccc5b69
Dmitry374 <dmitry.turov374@gmail.com>
2016-11-23 01:05:22 +0300

    create Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,16 @@
 public class Calculate{
     public static void main(String[] args) {
         System.out.println("Calculate ...");
+
+        int first = Integer.valueOf(args[0]);
+        int second = Integer.valueOf(args[1]);
+        int sum = first + second;
+        int mul = first * second;
+        int dif = first - second;
+        double div = first/second;
+        System.out.println("sum = " + sum);
+        System.out.println("mul = " + mul);
+        System.out.println("dif = " + dif);
+        System.out.println("div = " + div);
     }
 }
